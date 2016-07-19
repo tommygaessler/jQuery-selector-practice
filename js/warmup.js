79bd9cde2d298bce9961585f39f1d1be1b6b1939
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-07-19 09:48:44 -0600

    finished warmup

--- js/warmup.js
+++ js/warmup.js
@@ -6,50 +6,51 @@ window.Warmup = {
       Description: show the names of the people at the top level
       Outcome: Izaiah, Eulalia
     */
+    console.log("test");
     showNamesForSelector("#top-level-button", "#root > li > span");
 
     /*
       Description: show the names of the people at the second level
       Outcome: Joel, Aaliyah, Claude
     */
-    showNamesForSelector("#second-level-button", ___);
+    showNamesForSelector("#second-level-button", "#root > li > ul > li > span");
     //                       You need to replace ^^^ with a valid CSS selector
 
     /*
       Description: show the names of the people who have data-employment="full-time"
       Outcome: Izaiah, Della, Lowell
     */
-    showNamesForSelector("#full-time-button", ___);
+    showNamesForSelector("#full-time-button", '[data-employment]');
 
     /*
       Description: show the names of the people with the class name "talkative"
       Outcome: Claude, Lurline
     */
-    showNamesForSelector("#talkative-button", ___);
+    showNamesForSelector("#talkative-button", '.talkative');
 
     /*
       Description: show the names of the people who have an id of "vp"
       Outcome: Eulalia
     */
-    showNamesForSelector("#vp-button", ___);
+    showNamesForSelector("#vp-button", '#vp');
 
     /*
       Description: show the names of the people under Isaiah
       Outcome: Joel, Della, Lurline, Aaliyah, Elisa
     */
-    showNamesForSelector("#under-isaiah-button", ___);
+    showNamesForSelector("#under-isaiah-button", "#root > li:first-of-type > ul");
 
     /*
       Description: show the names of the people under Eulalia
       Outcome: Claude, Lowell
     */
-    showNamesForSelector("#under-eulalia-button", ___);
+    showNamesForSelector("#under-eulalia-button", "#root > li:last-of-type > ul");
 
     /*
       Description: values of all of the input fields of type text
       Outcome: Joe, Example
     */
-    showValuesForSelector("#text-field-button", ___);
+    showValuesForSelector("#text-field-button", 'form > label > input:text');
 
   }
 
